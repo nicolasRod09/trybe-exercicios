166af89619f767b8859b40b908e002a7a6a4506c
nicolasRod09 <nico.srod@hotmail.com>
2023-01-26 21:18:35 -0300

    adiciona maior de tres numeros

--- 1_fundamentos/introducao-a-javascript/dia01-1-Java-script-primeiros-passos/script.js
+++ 1_fundamentos/introducao-a-javascript/dia01-1-Java-script-primeiros-passos/script.js
@@ -1,9 +1,20 @@
 const numero1 = 20;
 const numero2 = 21;
+const numero3 = 22;
+
+//if (numero1 > numero2) {
+//    console.log(numero1);
+//}
+//else {
+//    console.log(numero2);
+//}
 
 if (numero1 > numero2) {
     console.log(numero1);
 }
-else {
+else if (numero2 > numero3) {
     console.log(numero2);
+}
+else {
+    console.log(numero3);
 }
