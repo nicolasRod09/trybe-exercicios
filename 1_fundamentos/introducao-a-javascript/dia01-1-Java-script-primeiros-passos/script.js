66fb844d2bf2c594c71e6e927951cf2afead642f
nicolasRod09 <nico.srod@hotmail.com>
2023-01-29 12:22:37 -0300

    termina exercicios else if/ switch case

--- 1_fundamentos/introducao-a-javascript/dia01-1-Java-script-primeiros-passos/script.js
+++ 1_fundamentos/introducao-a-javascript/dia01-1-Java-script-primeiros-passos/script.js
@@ -1,20 +1,88 @@
-const numero1 = 20;
-const numero2 = 21;
-const numero3 = 22;
+const numero1 = 60;
+const numero2 = 60;
+const numero3 = 40;
 
-//if (numero1 > numero2) {
+//Exercício1
+
+// if (numero1 > numero2) {
 //    console.log(numero1);
 //}
-//else {
+// else {
 //    console.log(numero2);
 //}
 
-if (numero1 > numero2) {
-    console.log(numero1);
-}
-else if (numero2 > numero3) {
-    console.log(numero2);
-}
-else {
-    console.log(numero3);
-}
+// Exercício2
+
+// if (numero1 > numero2) {
+//   console.log(numero1);
+//}
+// else if (numero2 > numero3) {
+//        console.log(numero2);
+//}
+// else {
+//    console.log(numero3);
+//}
+
+// Exercício3
+
+// if (numero1 + numero2 + numero3 === 180) {
+//    console.log(true);
+// }
+// else {
+//    console.log(false);
+// }
+
+// Exercício4
+
+// const chessPiece = 'Rei';
+
+// switch (chessPiece.toLowerCase()) {
+//     case "rei":
+//         console.log("Rei -> move apenas uma casa pra qualquer direção");
+//         break
+//     case "peão":
+//         console.log("Peão -> move apenas uma cas para frente");
+//         break
+//     case "rainha":
+//         console.log("Rainha -> move para qualquer casa livre que esteja alinhada com ela");
+//         break
+//     case "cavalo":
+//         console.log("Cavalo -> move em L para sendo 5 casas desde a ponta do l até a casa desejada");
+//         break
+//     case "torre":
+//         console.log("Torre -> move em qualquer sentido, tendo possibilidade de atravessar o tabuleiro inteiro, desde que nenhuma outra peça obstrua seu caminho");
+//         break
+//     case "bispo":
+//         console.log("Bispo -> move apenas em diagonal");
+//         break
+// }
+
+// Exercício 5
+
+// const porcentagemNota = 85;
+
+// if (porcentagemNota < 0 || porcentagemNota > 100) {
+//     console.log("Erro, nota incorreta!")
+// } else if (porcentagemNota >= 90) {
+//     console.log("Parabéns você tirou A");
+// } else if (porcentagemNota >= 80) {
+//     console.log("Parabéns você tirou B");
+// } else if (porcentagemNota >= 70) {
+//     console.log("Você tirou C");
+// } else if (porcentagemNota >= 60) {
+//     console.log("Você tirou D");
+// } else if (porcentagemNota >= 50) {
+//     console.log("Você tirou E");
+// } else {
+//     console.log("Você tirou F");
+// }
+
+// const salarioBruto = 6600.10;
+
+// if (salarioBruto <= 1556.94) {
+//     console.log("Alíquota de 8%");
+// } else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+//     console.log("Alíquota de 9%");
+// } else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+//     console.log("Alíquota de 11%");
+// } else if {
