82388e455c18ab6c484115238eeea457b1272efe
nicolasRod09 <nico.srod@hotmail.com>
2023-02-01 16:22:03 -0300

    adicionar sem urgencia background

--- seletores/script.js
+++ seletores/script.js
@@ -9,3 +9,6 @@ for (let index = 0; index < cabecalhoUrgente.length; index += 1) {
     cabecalhoUrgente[index].style.backgroundColor = 'rgb(165, 0, 243)';
 }
 
+const taskSemUrgencia = document.querySelector('.no-emergency-tasks');
+taskSemUrgencia.style.backgroundColor = 'rgb(249, 219, 94)';
+
