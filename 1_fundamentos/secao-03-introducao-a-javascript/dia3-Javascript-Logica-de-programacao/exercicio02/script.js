78dc5b8cbfb7c18d2c365ef3d2d39b503920bdb9
nicolasRod09 <nico.srod@hotmail.com>
2023-02-04 12:08:32 -0300

    adiciona for para maior e menor

--- 1_fundamentos/secao-03-introducao-a-javascript/dia3-Javascript-Logica-de-programacao/exercicio02/script.js
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia3-Javascript-Logica-de-programacao/exercicio02/script.js
@@ -17,10 +17,18 @@ console.log(reverse);
 
 let array = ["java", "javascript", "python", "html", "css"];
 let biggestValue = array[0];
-let smallestVallue = array[0];
+let smallestValue = array[0];
 
-for (index = 0; index < array.length; index += 1) {
-    if (array.length < smallestVallue) {
-
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length > biggestValue.length) {
+        biggestValue = array[index];
     }
 }
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length < smallestValue.length) {
+        smallestValue = array[index];
+    }
+}
+console.log(biggestValue);
+console.log(smallestValue);
