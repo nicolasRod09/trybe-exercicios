56df76e47647f7f0600afd07df2f8d520df1122e
nicolasRod09 <nico.srod@hotmail.com>
2023-02-04 18:08:19 -0300

    Adiciona variavel player

--- 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exercicio1/script.js
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exercicio1/script.js
@@ -9,5 +9,30 @@ let diasDaSemana = {
 };
 
 //   diasDaSemana.1; // SyntaxError: Unexpected number
-console.log(diasDaSemana['1']); // domingo
+console.log(diasDaSemana['7']); // domingo
 
+let conta = {
+    agencia: '0000',
+    banco: {
+        cod: '012',
+        id: 4,
+        nome: 'TrybeBank',
+    },
+};
+
+let infoDoBanco = 'banco';
+console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
+console.log(conta[infoDoBanco]['nome']); // TrybeBank
+
+console.log(conta.agencia); // 0000
+console.log(conta['agencia']); // 0000
+
+console.log(conta.banco.cod); // 012
+console.log(conta['banco']['id']); // 4
+
+let player = {
+    name: "Nicolas",
+    lastName: "Rodrigues",
+    age: 19,
+    medals: { golden: 2, silver: 3 }
+}
