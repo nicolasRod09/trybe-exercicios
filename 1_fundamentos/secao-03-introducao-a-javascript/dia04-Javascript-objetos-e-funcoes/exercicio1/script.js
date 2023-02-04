9c719497e51ad2b73b3e4ada65c2880db17e03ef
nicolasRod09 <nico.srod@hotmail.com>
2023-02-04 18:36:49 -0300

    adiciona frase

--- 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exercicio1/script.js
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exercicio1/script.js
@@ -33,10 +33,10 @@ console.log(conta['banco']['id']); // 4
 let player = {
     name: "Marta",
     lastName: "Silva",
-    age: 19,
+    age: 34,
     medals: { golden: 2, silver: 3 }
 };
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " " + "anos de idade");
 
 player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
-console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor jogadora do mundo por 6 vezes. " + player.bestInTheWorld);
+console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor jogadora do mundo " + player["bestInTheWorld"].length + " vezes" + "(" + player.bestInTheWorld + ").");
