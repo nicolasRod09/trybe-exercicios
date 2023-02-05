e6e602c4238174644502fb318b61fbed0a28a0ed
nicolasRod09 <nico.srod@hotmail.com>
2023-02-05 17:44:42 -0300

    adiciona eexercicio maiorDe3

--- 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exerciciofunções/script.js
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exerciciofunções/script.js
@@ -1,23 +1,45 @@
 const a = 10;
 const b = 35;
+const c = 35.5;
 
-function soma(a, b) {
-    return a + b;
-}
-soma();
 
-function sub(a, b) {
-    return a - b;
+// function soma(a, b) {
+//     return a + b;
+// }
+// soma();
+
+// function sub(a, b) {
+//     return a - b;
+// }
+
+// function mult(a, b) {
+//     return a * b;
+// }
+
+// function div(a, b) {
+//     return a / b;
+// }
+
+// function mod(a, b) {
+//     return a % b;
+// }
+
+function maior(a, b) {
+    if (a > b) {
+        return a + " é maior que " + b;
+    } else {
+        return b + " é maior que " + b;
+    }
 }
 
-function mult(a, b) {
-    return a * b;
+function maiorDe3(a, b, c) {
+    if (a > b && a > c) {
+        return "O maior número é " + a;
+    } else if (a < b && b > c) {
+        return "O maior número é " + b;
+    } else {
+        return "O maior número é " + c;
+    }
 }
 
-function div(a, b) {
-    return a / b;
-}
-
-function mod(a, b) {
-    return a % b;
-}
+console.log(maiorDe3(a, b, c));
