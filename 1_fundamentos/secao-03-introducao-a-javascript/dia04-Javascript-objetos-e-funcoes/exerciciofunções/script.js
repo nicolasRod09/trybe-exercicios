a3e186c5f1ec5d2a382400a8335b230c41ce775a
nicolasRod09 <nico.srod@hotmail.com>
2023-02-05 17:02:38 -0300

    adiciona exercicio

---
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exerciciofunções/script.js
@@ -0,0 +1,23 @@
+const a = 10;
+const b = 35;
+
+function soma(a, b) {
+    return a + b;
+}
+soma();
+
+function sub(a, b) {
+    return a - b;
+}
+
+function mult(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function mod(a, b) {
+    return a % b;
+}
