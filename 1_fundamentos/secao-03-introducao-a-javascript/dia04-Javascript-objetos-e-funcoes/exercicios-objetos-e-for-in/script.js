6b1201760e63ae93c79a293b09fe286d5ac1b131
nicolasRod09 <nico.srod@hotmail.com>
2023-02-05 23:09:45 -0300

    adiciona restante de for in

--- 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exercicios-objetos-e-for-in/script.js
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia04-Javascript-objetos-e-funcoes/exercicios-objetos-e-for-in/script.js
@@ -4,7 +4,7 @@ let info = {
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
 };
 
-let infor = {
+let info2 = {
     personagem: "Tio Patinhas",
     origem: "Christmas on Bear Mountain",
     nota: "Dell's Four Color Comics #178",
@@ -17,9 +17,24 @@ info["recorrente"] = "Sim";
 
 // console.log(info);
 
-for (key in info) {
-    console.log(key + ": " + info[key]);
-}
+// for (key in info) {
+//     console.log(key + ": " + info[key]);
+// }
 
+// for (let key in info2) {
+//     console.log(info2[key]);
+// }
+
+for (let key in info) {
+    if (
+        key === "recorrente" &&
+        info[key] === "Sim" &&
+        info2[key] === "Sim"
+    ) {
+        console.log("Ambos recorrentes");
+    } else {
+        console.log(info[key] + " e " + info2[key]);
+    }
+}
 
 
