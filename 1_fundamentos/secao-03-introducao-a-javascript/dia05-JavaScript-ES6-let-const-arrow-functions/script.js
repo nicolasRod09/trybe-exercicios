d5d767938fe4c0fb6576062ae7762daf080655e5
nicolasRod09 <nico.srod@hotmail.com>
2023-02-06 21:56:44 -0300

    adicionar arrow function math random

--- 1_fundamentos/secao-03-introducao-a-javascript/dia05-JavaScript-ES6-let-const-arrow-functions/script.js
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia05-JavaScript-ES6-let-const-arrow-functions/script.js
@@ -35,4 +35,10 @@ function soma(a, b) {
 let a = 3;
 let b = 5;
 // console.log('O resultado da soma de ' + a + ' + ' + b + ' é: ' + soma(a, b));
+
 console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a, b)}`);
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+const numeroAleatorio = () => Math.random();
+console.log(numeroAleatorio());
