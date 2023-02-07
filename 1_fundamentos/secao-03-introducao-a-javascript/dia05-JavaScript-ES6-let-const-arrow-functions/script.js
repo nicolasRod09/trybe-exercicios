b4fe69c7a8d207ed12e8129d039467720a899210
nicolasRod09 <nico.srod@hotmail.com>
2023-02-06 21:34:35 -0300

    adiciona novos exercicios

--- 1_fundamentos/secao-03-introducao-a-javascript/dia05-JavaScript-ES6-let-const-arrow-functions/script.js
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia05-JavaScript-ES6-let-const-arrow-functions/script.js
@@ -17,6 +17,21 @@ pessoa.idade = 19;
 console.log('Nome:', pessoa.nome);
 console.log('Idade:', pessoa.idade);
 
+///////////////////////////////////////////////////////////////////////////////////////
+
 let favoriteFood = 'Lasanha';
 favoriteFood = 'Hamburguer';
 console.log(favoriteFood);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const name = 'Adriana';
+const lastName = 'Soares';
+console.log(`Olá, ${name} ${lastName}!`);
+function soma(a, b) {
+    let resultado = a + b;
+    return resultado;
+}
+let a = 3;
+let b = 5;
+console.log('O resultado da soma de ' + a + ' + ' + b + ' é: ' + soma(a, b));
