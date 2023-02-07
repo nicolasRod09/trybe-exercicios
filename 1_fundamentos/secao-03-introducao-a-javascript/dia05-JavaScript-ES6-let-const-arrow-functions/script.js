ee87253b5ec5fe511e2919e8d40fc0498e3b2a70
nicolasRod09 <nico.srod@hotmail.com>
2023-02-06 21:14:27 -0300

    adiciona exercicios dia 5

---
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia05-JavaScript-ES6-let-const-arrow-functions/script.js
@@ -0,0 +1,22 @@
+function imprimeIdade() {
+    for (let idade = 30; idade <= 40; idade += 1) {
+        console.log('Idade dentro do for:', idade)
+    }
+}
+imprimeIdade()
+
+/////////////////////////////////////////////////////////////////////////
+
+// Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+const pessoa = {
+    nome: 'Henri',
+    idade: 20
+}
+pessoa.nome = 'Luna';
+pessoa.idade = 19;
+console.log('Nome:', pessoa.nome);
+console.log('Idade:', pessoa.idade);
+
+let favoriteFood = 'Lasanha';
+favoriteFood = 'Hamburguer';
+console.log(favoriteFood);
