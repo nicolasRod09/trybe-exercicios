de5182b5a9d8230d07febcc98861f98aedfcf46e
nicolasRod09 <nico.srod@hotmail.com>
2023-02-13 19:07:01 -0300

    arruma documentos

--- 1_fundamentos/secao-03-introducao-a-javascript/dia3-Javascript-Logica-de-programacao/exercicio02/script.js
+++ 1_fundamentos/secao-03-introducao-a-javascript/dia3-Javascript-Logica-de-programacao/exercicio02/script.js
@@ -15,34 +15,34 @@ for (let index = 0; index < word.length; index += 1) {
 
 console.log(reverse);
 
-// let array = ["java", "javascript", "python", "html", "css"];
-// let biggestValue = array[0];
-// let smallestValue = array[0];
+let array = ["java", "javascript", "python", "html", "css"];
+let biggestValue = array[0];
+let smallestValue = array[0];
 
-// for (let index = 0; index < array.length; index += 1) {
-//     if (array[index].length > biggestValue.length) {
-//         biggestValue = array[index];
-//     }
-// }
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length > biggestValue.length) {
+        biggestValue = array[index];
+    }
+}
 
 // for (let index = 0; index < array.length; index += 1) {
 //     if (array[index].length < smallestValue.length) {
 //         smallestValue = array[index];
 //     }
-// }
-// console.log(biggestValue);
+// } 
+console.log(biggestValue);
 // console.log(smallestValue);
 
-let maiorNumero = 0;
+// let maiorNumero = 0;
 
-for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
-    let isPrime = true;
-    for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
-        if (currentNumber % currentDivisor === 0) {
-            isPrime = false;
-        }
-    }
-    if (isPrime) {
-        maiorNumero = currentNumber;
-    }
-}
+// for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+//     let isPrime = true;
+//     for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+//         if (currentNumber % currentDivisor === 0) {
+//             isPrime = false;
+//         }
+//     }
+//     if (isPrime) {
+//         maiorNumero = currentNumber;
+//     }
+// }
