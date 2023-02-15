0cd5faff3d152def0e99df92df764a9f6a5ae7e0
nicolasRod09 <nico.srod@hotmail.com>
2023-02-14 22:59:13 -0300

    adiciona restante de exercicios

--- 1_fundamentos/javascript-dom-eventos-e-web-storage/dia-03-Javascript-eventos-e-web-storage/main.js
+++ 1_fundamentos/javascript-dom-eventos-e-web-storage/dia-03-Javascript-eventos-e-web-storage/main.js
@@ -16,16 +16,30 @@ const apagaTechEaddOutra = (event) => {
     techElement.target.classList.add('tech');
     input.value = '';
 }
+firstLi.addEventListener('click', apagaTechEaddOutra);
+secondLi.addEventListener('click', apagaTechEaddOutra);
+thirdLi.addEventListener('click', apagaTechEaddOutra);
 // - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
-
+input.addEventListener('input', (event) => {
+    const techElement = document.querySelector('.tech');
+    techElement.innerText = event.target.value;
+});
 // - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 //  - Que tal redirecionar para seu portfólio?
-
+myWebpage.addEventListener('dblclick', () => {
+    window.location.replace('https://nicolasrod09.github.io')
+})
 // - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+})
 
+myWebpage.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'unset';
+})
 // Segue abaixo um exemplo do uso de event.target:
 
 
