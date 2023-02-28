79a3975270df31ae2d4dbe2fb46ebd18503006d9
nicolasRod09 <nico.srod@hotmail.com>
2023-02-28 18:24:29 -0300

    adiciona index e script

--- 1_fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
+++ 1_fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-04-javascript-web-storage/script.js
@@ -1,9 +1,25 @@
-console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
-sessionStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
-sessionStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
-console.log(sessionStorage.getItem('lastname')); // retorna o valor 'Smith'
-console.log(sessionStorage.length); // possui duas entradas, então o retorno é o valor: 2
-sessionStorage.removeItem('lastname'); // remove a entrada referente a key = 'lastname'
-console.log(sessionStorage.length); // possui uma entrada, então o retorno é o valor: 1
-sessionStorage.clear(); // limpa todas as entradas salvas em sessionStorage
-console.log(sessionStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
+
+const insertPhraseInDOM = () => {
+    const phrasesList = JSON.parse(sessionStorage.getItem('phrases'));
+    const listLength = phrasesList.length - 1;
+    const phraseText = phrasesList[listLength];
+    const phrase = document.createElement('li');
+    phrase.innerText = phraseText;
+    list.appendChild(phrase);
+}
+
+const addPhraseToSessionStorage = () => {
+    if (sessionStorage.getItem('phrases') === null) {
+        sessionStorage.setItem('phrases', JSON.stringify([]));
+    }
+    const oldList = JSON.parse(sessionStorage.getItem('phrases'));
+    const phraseText = input.value;
+    oldList.push(phraseText);
+    sessionStorage.setItem('phrases', JSON.stringify(oldList));
+    insertPhraseInDOM();
+}
+
+button.addEventListener('click', addPhraseToSessionStorage);
